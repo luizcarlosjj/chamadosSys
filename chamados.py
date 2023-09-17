8da09c095ee8580710ef1a919db0196105915431
Luiz Carlos <dia.a.diajj@gmail.com>
2023-09-16 23:07:34 -0300

    update

--- chamados.py
+++ chamados.py
@@ -2,12 +2,16 @@ from PyQt5 import uic,QtWidgets
 import sqlite3
 
 # Login Usuários
+def showLogin():
+    home.frame_Login.show()
+    home.frame_Cadastro.close()
+    home.frame_off_login.close()
 def login_task():
-    nome_usuario = login.user.text()
-    senha = login.passwd.text()
-    tipo = login.combobox.currentText()
+    nome_usuario = home.user.text()
+    senha = home.passwd.text()
+    tipo = home.tipoLog.currentText()
     tec = str('tecnico')
-    clie = str('usuario')
+    user = str('usuario')
     banco = sqlite3.connect("db/banco_chamados.db")
     cursor = banco.cursor()
     try:
@@ -19,26 +23,36 @@ def login_task():
     except:
         print('erro, usuario inválido.')
 
-    if senha == senha_bd[0][0] and tipo_db[0][0] == tipo and tipo_db[0][0] == clie:
-        tela_cliente.show()
-        login.user.setText("")
-        login.passwd.setText(" ")
-        login.close()
-        login.label_6.setText(" ")
-    elif senha == senha_bd[0][0] and tipo_db[0][0] == tipo and tipo_db[0][0] == tec:
-        tela_tecnico.show()
-        login.user.setText("")
-        login.passwd.setText(" ")
-        login.close()
-        login.label_6.setText(" ")
+    if senha == senha_bd[0][0] and tipo_db[0][0] == tipo: #and tipo_db[0][0] == user
+        inicio.show()
+        home.user.setText("")
+        home.passwd.setText("")
+        home.close()
+        home.label_6.setText("")
     else:
-        login.label_6.setText("Dados de login incorretos")
+        home.label_6.setText("Dados de login incorretos")
+    '''elif senha == senha_bd[0][0] and tipo_db[0][0] == tipo and tipo_db[0][0] == tec:
+        inicio.show()
+        home.user.setText("")
+        home.passwd.setText("")
+        home.close()
+        home.label_6.setText("")'''
+
+
+    
+    return tipo_db
+
 # Cadastro Usuários
+def showCadastro():
+    home.frame_Cadastro.show()
+    home.frame_Login.close()
+    home.frame_off_login.close()
+    home.resCad.setText("")
 def cadastro_task():
-    nome = tela_cadastro_user.user.text()
-    senha = tela_cadastro_user.user_2.text()
-    filial = tela_cadastro_user.filial.currentText()
-    tipo = tela_cadastro_user.tipo.currentText()
+    nome = home.nameCad.text()
+    senha = home.senhaCad.text()
+    tipo = home.tipoCad.currentText()
+    filial = home.filialCad.currentText()
 
     if (len(senha) > 0):
         try:
@@ -48,26 +62,56 @@ def cadastro_task():
             cursor.execute("INSERT INTO cadastros VALUES ('"+nome+"','"+filial+"','"+tipo+"', '"+senha+"')")
             banco.commit()
             banco.close()
-            tela_cadastro_user.label_3.setText("Usuário cadastrado com sucesso!")
+            home.resCad.setText("Usuário cadastrado com sucesso! \n Realize Login")
+            home.nameCad.setText("")
+            home.senhaCad.setText("")
+            home.repSenhaCad.setText("")
+            itemInicial = ""
+            home.filialCad.setCurrentText(itemInicial)
+            home.tipoCad.setCurrentText(itemInicial)
+
 
         except sqlite3.Error as erro:
             print("Erro ao inserir os dados: ", erro)
     else:
-        tela_cadastro_user.label_3.setText("Digite uma senha válida!")
-# Cadastro Chamados
+        home.resCad.setText("Digite uma senha válida!")
+
+
+# INICIO
+def login_CadChamados():
+    chamados.show()
+    inicio.label_3.setText("")
+def login_TratChamados():
+    tipo = home.tipoLog.currentText()
+    if tipo == 'usuario':
+        inicio.label_3.setText('Usuário sem permissão para está ação!')
+    elif tipo == 'tecnico':
+        tela_tratar_chamados.show()
+def login_Stats():
+    tela_estatisticas.show()
+    inicio.label_3.setText("")
+def logout_inicio():
+    inicio.close()
+    home.show()
+    item = ""
+    home.tipoLog.setCurrentText(item)
+    home.label_6.setText("")
+
+
+# CADASTRO DE CHAMADOS:
 def chamados_task():
-    if tela_cadastro_chamados.radioButton.isChecked():
+    if chamados.radioButton.isChecked():
         opcao = ('Manutenção')
-    elif tela_cadastro_chamados.radioButton_2.isChecked():
+    elif chamados.radioButton_2.isChecked():
         opcao = ('Suporte')
-    elif tela_cadastro_chamados.radioButton_3.isChecked():
+    elif chamados.radioButton_3.isChecked():
         opcao = ('Dev')
     else: 
         opcao = ""
 
     status = str('Aberto')
-    nome = tela_cadastro_chamados.lineEdit_2.text()
-    descricao = tela_cadastro_chamados.lineEdit.text()
+    nome = chamados.nameCmd.text()
+    descricao = chamados.descCmd.text()
     categoria = str(opcao)
 
     banco = sqlite3.connect('db/banco_chamados.db')
@@ -84,26 +128,88 @@ def chamados_task():
     cursor.execute("INSERT INTO chamados (nome_solicitante, descricao, categoria, status) VALUES ('"+nome+"', '"+descricao+"', '"+categoria+"', '"+status+"')")
     banco.commit()
     banco.close()
-    tela_cadastro_chamados.label_6.setText('Chamado Cadastrado!')
-    tela_cadastro_chamados.lineEdit_2.setText('')
-    tela_cadastro_chamados.lineEdit.setText('')
-# Tratamento de Chamados
-def tratar_task():
+    chamados.label_6.setText('Chamado Cadastrado!')
+    chamados.nameCmd.setText('')
+    chamados.descCmd.setText('')
+    chamados.filialAtd.setCurrentText("")
+def logout_chamados():
+    chamados.close()
+
+
+# TRATAMENTO DE CHAMADOS:
+def chamados_View_Select():
     tela_tratar_chamados.show()
+    tela_tratar_chamados.res_Select.setText("")
+    tela_tratar_chamados.frame_select.show()
+    tela_tratar_chamados.frame_conclui.close()
+    tela_tratar_chamados.frame_off.close()
     banco = sqlite3.connect('db/banco_chamados.db')
     cursor = banco.cursor()
     cursor.execute("SELECT * FROM chamados")
     dados_lidos = cursor.fetchall()
 
-    tela_tratar_chamados.tableWidget.setRowCount(len(dados_lidos))
-    tela_tratar_chamados.tableWidget.setColumnCount(5)
-    tela_tratar_chamados.tableWidget.setHorizontalHeaderLabels(["id","Nome do Solicitante","Descição", "categoria", "Status"])
+    tela_tratar_chamados.tableWidgetSelect.setRowCount(len(dados_lidos))
+    tela_tratar_chamados.tableWidgetSelect.setColumnCount(5)
+    tela_tratar_chamados.tableWidgetSelect.setHorizontalHeaderLabels(["id","Nome do Solicitante","Descição", "categoria", "Status"])
 
     rows = dados_lidos
     for i in range(len(rows)): #linha
         for j in range(len(rows[0])): #coluna
             item = QtWidgets.QTableWidgetItem(f"{rows[i][j]}")
-            tela_tratar_chamados.tableWidget.setItem(i,j, item)    
+            tela_tratar_chamados.tableWidgetSelect.setItem(i,j, item) 
+def chamados_View_Concluir():
+    tela_tratar_chamados.show()
+    tela_tratar_chamados.res_Conclui.setText("")
+    tela_tratar_chamados.frame_conclui.show()
+    tela_tratar_chamados.frame_select.close()
+    tela_tratar_chamados.frame_off.close()
+    banco = sqlite3.connect('db/banco_chamados.db')
+    cursor = banco.cursor()
+    cursor.execute("SELECT * FROM chamados")
+    dados_lidos = cursor.fetchall()
+
+    tela_tratar_chamados.tableWidgetConcluir.setRowCount(len(dados_lidos))
+    tela_tratar_chamados.tableWidgetConcluir.setColumnCount(5)
+    tela_tratar_chamados.tableWidgetConcluir.setHorizontalHeaderLabels(["id","Nome do Solicitante","Descição", "categoria", "Status"])
+
+    rows = dados_lidos
+    for i in range(len(rows)): #linha
+        for j in range(len(rows[0])): #coluna
+            item = QtWidgets.QTableWidgetItem(f"{rows[i][j]}")
+            tela_tratar_chamados.tableWidgetConcluir.setItem(i,j, item) 
+def voltar():
+    tela_tratar_chamados.close()
+    tela_tratar_chamados.label_3.setText('')
+def limpar():
+    tela_tratar_chamados.frame_conclui.close()
+    tela_tratar_chamados.frame_select.close()
+    tela_tratar_chamados.frame_off.show()
+
+
+def selecionar_chamados():
+    banco = sqlite3.connect("db/banco_chamados.db")
+    cursor = banco.cursor()
+    id_digitado = int(tela_tratar_chamados.id_Select.text())
+    cursor.execute("UPDATE chamados SET status='Em tratamento' WHERE id={}".format(id_digitado))
+    banco.commit()
+    banco.close()
+    tela_tratar_chamados.res_Select.setText('Atribuido com sucesso!')
+    tela_tratar_chamados.id_Select.setText("")
+    chamados_View_Select()
+
+def concluir_chamados():
+    banco = sqlite3.connect("db/banco_chamados.db")
+    cursor = banco.cursor()
+    id_digitado = int(tela_tratar_chamados.id_Conclui.text())
+    cursor.execute("UPDATE chamados SET status='Concluido' WHERE id={}".format(id_digitado))
+    banco.commit()
+    banco.close()
+    tela_tratar_chamados.res_Conclui.setText('Concluido com sucesso!')
+    tela_tratar_chamados.id_Conclui.setText("")
+    chamados_View_Concluir()
+
+
+
 # Mostrar Estatisticas
 def estatisticas_show():
     tela_estatisticas.show()
@@ -128,120 +234,50 @@ def estatisticas_show():
 
     banco.commit()
     banco.close()
-# Selecionar Chamados
-def selecionar_chamados():
-    banco = sqlite3.connect("db/banco_chamados.db")
-    cursor = banco.cursor()
-    id_digitado = int(tela_selecionar_chamados.lineEdit.text())
-    cursor.execute("UPDATE chamados SET status='Em tratamento' WHERE id={}".format(id_digitado))
-    banco.commit()
-    banco.close()
-    tela_selecionar_chamados.label_2.setText('Atribuido com sucesso!')
-    tela_selecionar_chamados.lineEdit.setText("")
-# Concluir chamados
-def alterar_status_chamados():
-    banco = sqlite3.connect("db/banco_chamados.db")
-    cursor = banco.cursor()
-    id_digitado = int(tela_concluir_chamados.lineEdit.text())
-    cursor.execute("UPDATE chamados SET status='Concluido' WHERE id={}".format(id_digitado))
-    banco.commit()
-    banco.close()
-    tela_concluir_chamados.label_2.setText('Concluido com sucesso!')
-    tela_concluir_chamados.lineEdit.setText("")
-    
 
-# abertura de telas:
-def abrir_cadastro_user():
-    login.close()
-    tela_cadastro_user.show()
-def abrir_cadastro_chamados():
-    tela_cadastro_chamados.show()
-def abrir_tratamento():
-    tela_tratar_chamados.show()
-def abrir_concluir_chamados():
-    tela_concluir_chamados.show()
-def abrir_selecionar_chamados():
-    tela_selecionar_chamados.show()
-
-# fechamento de telas:
-def logout_cadastro_user():
-    tela_cadastro_user.close()
-    tela_cadastro_user.label_3.setText("")
-    tela_cadastro_user.user.setText("")
-    login.show()
-def logout_tela_cliente():
-    tela_cliente.close()
-    login.show()
-def logout_tela_tecnico():
-    tela_tecnico.close()
-    login.show()
-def logout_tela_chamado():
-    tela_cadastro_chamados.close()
-    tela_cadastro_chamados.close()
-def logout_chamados():
-    tela_tratar_chamados.close()
 def logout_estatisticas():
     tela_estatisticas.close()
-def logout_concluir_chamados():
-    tela_concluir_chamados.close()
-    tela_tratar_chamados.show()
-def logout_selecionar_chamados():
-    tela_selecionar_chamados.close()
-    tela_tratar_chamados.show()
+
 
 app = QtWidgets.QApplication([])
-
 # import Telas
-login = uic.loadUi("pages/login.ui")
-tela_cadastro_user = uic.loadUi("pages/cadastro.ui")
-tela_cliente = uic.loadUi("pages/home_cliente.ui")
-tela_tecnico = uic.loadUi("pages/home_tec.ui")
-tela_cadastro_chamados = uic.loadUi("pages/cad_chamados.ui")
+home = uic.loadUi("pages/login.ui")
+inicio = uic.loadUi("pages/inicio.ui")
+chamados = uic.loadUi("pages/chamadosCad.ui")
 tela_tratar_chamados = uic.loadUi("pages/tratar_chamados.ui")
 tela_estatisticas = uic.loadUi("pages/stats_chamados.ui")
-tela_concluir_chamados = uic.loadUi("pages/concluir_chamados.ui")
-tela_selecionar_chamados = uic.loadUi("pages/selecionar_chamados.ui")
 
-# tela LOGIN
-login.pushButton_2.clicked.connect(login_task)
-login.pushButton_3.clicked.connect(abrir_cadastro_user)
+# tela HOME
+home.frame_off_login.show()
+home.btn_Login.clicked.connect(showLogin) # MOSTRA ABA DE LOGIN
+home.btn_Entrar.clicked.connect(login_task) # EXECUTA LOGIN
+home.btn_Cadastro.clicked.connect(showCadastro) # MOSTRA ABA DE CADASTRO
+home.btn_Cad.clicked.connect(cadastro_task) # EXECUTA CADASTRO
+home.tipoCad.addItems(["","usuario","tecnico"]) # ADICIONA DADOS AO COMBO BOX "TIPO"
+home.filialCad.addItems(["","Lins 01","Lins 02"]) # ADICIONA DADOS AO COMBO BOX "FILIAL"
 
-# tela CADASTRO
-tela_cadastro_user.filial.addItems(["","Lins 01","Lins 02"])
-tela_cadastro_user.tipo.addItems(["","usuario","tecnico"])
-tela_cadastro_user.pushButton_3.clicked.connect(cadastro_task)
-tela_cadastro_user.pushButton_2.clicked.connect(logout_cadastro_user)
+# tela INICIO
+inicio.btnCadCmd.clicked.connect(login_CadChamados)
+inicio.btnTratCmd.clicked.connect(login_TratChamados)
+inicio.btnStats.clicked.connect(login_Stats)
+inicio.btnSairHome.clicked.connect(logout_inicio)
 
-# tela CLIENTE
-tela_cliente.pushButton.clicked.connect(abrir_cadastro_chamados)
-tela_cliente.pushButton_3.clicked.connect(logout_tela_cliente)
-tela_cliente.pushButton_4.clicked.connect(estatisticas_show)
-
-# tela TECNICO
-tela_tecnico.pushButton_2.clicked.connect(logout_tela_tecnico)
-tela_tecnico.pushButton_3.clicked.connect(tratar_task)
-tela_tecnico.pushButton_4.clicked.connect(estatisticas_show)
-
-# tela CRIAR CHAMADOS
-tela_cadastro_chamados.pushButton.clicked.connect(chamados_task)
-tela_cadastro_chamados.comboBox.addItems(["","Lins 01","Lins 02"])
-tela_cadastro_chamados.pushButton_2.clicked.connect(logout_tela_chamado)
+# tela CADASTAR CHAMADOS
+chamados.btnCadCmd.clicked.connect(chamados_task)
+chamados.btnSairCadCmd.clicked.connect(logout_chamados)
+chamados.filialAtd.addItems(["","Lins 01","Lins 02"])
 
 # tela TRATAR CHAMADOS
-tela_tratar_chamados.pushButton_2.clicked.connect(logout_chamados)
-tela_tratar_chamados.pushButton_3.clicked.connect(abrir_concluir_chamados)
-tela_tratar_chamados.pushButton_4.clicked.connect(abrir_selecionar_chamados)
+tela_tratar_chamados.frame_off.show()
+tela_tratar_chamados.btn_Select.clicked.connect(chamados_View_Select) # MOSTRAR ABA SELECIONAR CHAMADOS
+tela_tratar_chamados.btn_Concluir.clicked.connect(chamados_View_Concluir) # MOSTRAR ABA CONCLUIR CHAMADOS
+tela_tratar_chamados.btn_Limpar.clicked.connect(limpar) # LIMPAR TELA
+tela_tratar_chamados.btn_Voltar.clicked.connect(voltar) # VOLTAR
+
+tela_tratar_chamados.btn_SelectCmd.clicked.connect(selecionar_chamados) # EXECUTAR SELECT CHAMADOS
+tela_tratar_chamados.btn_ConcluirCmd.clicked.connect(concluir_chamados) # EXECUTAR CONCLUIR CHAMADOS
 
 # tela ESTATISTICAS
-tela_estatisticas.pushButton_2.clicked.connect(logout_estatisticas)
 
-# tela CONCLUIR CHAMADO
-tela_concluir_chamados.pushButton.clicked.connect(alterar_status_chamados)
-tela_concluir_chamados.pushButton_2.clicked.connect(logout_concluir_chamados)
-
-# tela SELECIONAR CHAMADO
-tela_selecionar_chamados.pushButton.clicked.connect(selecionar_chamados)
-tela_selecionar_chamados.pushButton_2.clicked.connect(logout_selecionar_chamados)
-
-login.show()
+home.show()
 app.exec()
